ffe3ba3c4517ba186ad2b4a71ce786265788acb4
nikpan1 <nikodem.panknin@gmail.com>
2024-01-26 19:27:52 +0100

    Small fix with import button.

--- GerberViewerQT/mainwindow.cpp
+++ GerberViewerQT/mainwindow.cpp
@@ -17,14 +17,14 @@ MainWindow::~MainWindow()
 void MainWindow::on_ImportButton_clicked()
 {
     QString filepath = QFileDialog::getOpenFileName(this, "Import Settings file:",
-                                                    QDir::rootPath(), "CSV Files (*csv))");
+                                                    QDir::rootPath(), "CSV Files (*.csv)");
 
     QFile mFile(filepath);
     QTextStream inFile(&mFile);
 
     int cter = 0;
     QString readedLine = inFile.readLine();
-    QString listValue = readedLine.split(",");
+    QStringList listValue = readedLine.split(",");
     ui->tableWidget->setHorizontalHeaderLabels(listValue);
 
 
