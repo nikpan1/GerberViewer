17f0372523e75fe2ccaba64c2287f346ad3393e4
nikpan1 <nikodem.panknin@gmail.com>
2024-01-26 19:43:00 +0100

    Adding new content to csv files.

--- GerberViewerQT/mainwindow.cpp
+++ GerberViewerQT/mainwindow.cpp
@@ -20,13 +20,33 @@ void MainWindow::on_ImportButton_clicked()
                                                     QDir::rootPath(), "CSV Files (*.csv)");
 
     QFile mFile(filepath);
+
+    if(!mFile.open(QFile::Text | QFile::ReadOnly)) {
+        return;
+    }
+
     QTextStream inFile(&mFile);
 
     int cter = 0;
     QString readedLine = inFile.readLine();
     QStringList listValue = readedLine.split(",");
-    ui->tableWidget->setHorizontalHeaderLabels(listValue);
 
+    ui->FeederTable->setColumnCount(listValue.size());
+    ui->FeederTable->setHorizontalHeaderLabels(listValue);
 
+    while(!inFile.atEnd()) {
+        QString readedLine = inFile.readLine();
+        QStringList listValue = readedLine.split(",");
+
+        cter += 1;
+        ui->FeederTable->setRowCount(cter);
+
+        for(int k = 0; k < listValue.size(); k ++ ) {
+            ui->FeederTable->setItem(cter - 1, k, new QTableWidgetItem(listValue[k]));
+        }
+
+        mFile.flush();
+        mFile.close();
+    }
 }
 
